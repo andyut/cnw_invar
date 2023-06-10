d131ae55fa70bad3447b8195be37a43891fb1aaa
Andy Utomo <andyut@outlook.com>
2023-06-10 13:05:32 +0700

    update

--- tf__models/tf_models.py
+++ tf__models/tf_models.py
@@ -9,7 +9,7 @@ import os
 import pytz
 from odoo.exceptions import UserError
 from odoo.modules import get_modules, get_module_path
-from datetime import datetime 
+from datetime import datetime, date, timedelta
 from jinja2 import Environment, FileSystemLoader
 import pdfkit
 import pyodbc  
@@ -285,56 +285,106 @@ class ARTukarfakturDetail(models.Model):
 class ARTukarfakturWizard(models.TransientModel):
 	_name           = "ar.tf.wizard"
 	_description    = "Tukar Faktur"
-	company_id      = fields.Many2one('res.company', 'Company', required=True, index=True,  default=lambda self: self.env.user.company_id.id)
-	name            = fields.Char("Nomor ID")
-	coll_id         = fields.Many2one("ar.collector",string="Collector")
-	jalur_id        = fields.Many2one("ar.jalur",string="Jalur")
-	remarks         = fields.Char("Remarks")
-	ar_id           = fields.Many2one("res.users",string="AR Person", default=lambda self: self.env.user.id )
-	docdate         = fields.Date("Date",default=lambda s:fields.Date.today())
-	statustf        = fields.Selection(string="Status",selection=[("Tukar Faktur","Tukar Faktur"),("Giro","Giro"),("Lain-lain","Lain Lain")],default="Tukar Faktur")       
+	company_id      = fields.Many2one('res.company', 'Company', required=True, index=True,  default=lambda self: self.env.user.company_id.id)  
+	tfdate         	= fields.Date("TukarFaktur",default=lambda s:fields.Date.today()) 
+	updatetf        = fields.Selection(string="Update",selection=[("tf","Update Tanggal Tukar Faktur"),("py","Update Tanggal Est Payment")],default="tf")       
+ 
+ 
+	def UpdateTglTf(self):
 
-	@api.multi
-	def CreateJadwalCollector(self):
+
+ 		 
+		NomorTF  = self.env["cnw.numbering.wizard"].getnumbering('TF',self.docdate)    
 		 
-		NomorTagihan  = self.env["cnw.numbering.wizard"].getnumbering('CNW-TF',self.docdate)    
-		ar_tf_id = self.env["ar.tf"].create({
-												"name"      : NomorTagihan,
-												"coll_id"   : self.coll_id.id,
-												"jalur_id"  : self.jalur_id.id ,
-												"remarks"   : self.remarks, 
-												"docdate"   : self.docdate
-											})
-		
 		listinvoice = self.env['ar.invoice'].browse(self._context.get('active_ids', []))
 		doctotal = 0.0
-		#print(listinvoice)
+
+#print(listinvoice)
+#########################
+# LOGIN
+#########################
+		CompanyDB 	= self.company_id.db_name
+		UserName 	= self.company_id.sapuser
+		Password 	= self.company_id.sappassword
+		url 		= self.company_id.sapsl
+
+
+		urllogin = url + "Login"
+		print(urllogin)
+
+		payload = { "CompanyDB" :CompanyDB ,
+					"UserName" : UserName,
+					"Password" : Password
+					}
+		
+		response = appSession.post(urllogin, json=payload,verify=False)
+		txtlog = ""
+		#print(response.json())
 		for invoice in listinvoice:
-			invoice.tf_id = ar_tf_id
-			invoice.docduedate = self.docdate
-			invoice.tf_date = self.docdate
-			invoice.tg_date = self.docdate
-			invoice.tf_number = NomorTagihan 
-			invoice.tf_type = self.statustf
-			invoice.tf_remarks = self.statustf
-			invoice.tf_collector =  self.coll_id.name
-			doctotal += invoice.total 
+			
+			paydate = self.tfdate + timedelta(days=invoice.topdays)
 		
-		ar_tf_id.doctotal = doctotal
+			appSession = requests.Session()
 
-		return { 
-				'res_model': 'ar.tf',
-				'res_id': ar_tf_id.id,
-				'type': 'ir.actions.act_window',
-				'context': {},
-				'view_mode': 'form',
-				'view_type': 'form', 
-				'target':'current'
 
-			}
+	#########################
+	# UPDATE TF
+	######################### 
+			if invoice.objtype =="13":
+				urltf = url + "Invoices("  + invoice.docentry + ")"
+				payload = {
+							"DocDueDate" : self.tfdate.strftime("%Y-%m-%d") , 
+							"TaxDate" : paydate.strftime("%Y-%m-%d") , 
+							"U_LT_No" : NomorTF ,
+							"U_TF_date" : self.tfdate.strftime("%Y-%m-%d"), 
+							"U_Tagihan_date" : self.tfdate.strftime("%Y-%m-%d"),
+						}               			
+				rsp = appSession.patch(urltf,json=payload,verify=False)
+				txtlog = txtlog + urltf + " >> " + str(rsp.status_code) +   "\n"
+			
+				if rsp.status_code >=400 :
+					print(urltf)
+					txtlog =txtlog + str(payload) + "\n"
+					print(txtlog =txtlog + str(rsp.json()) + "\n")
+
+			if invoice.objtype =="14":
+				urltf = url + "CreditNotes("  + invoice.docentry + ")"
+				payload = {
+							"DocDueDate" : self.tfdate.strftime("%Y-%m-%d") , 
+							"TaxDate" : paydate.strftime("%Y-%m-%d") , 
+							"U_LT_No" : NomorTF ,
+							"U_TF_date" : self.tfdate.strftime("%Y-%m-%d"), 
+							"U_Tagihan_date" : self.tfdate.strftime("%Y-%m-%d"),
+						}               			
+				rsp = appSession.patch(urltf,json=payload,verify=False)
+				txtlog = txtlog + urltf + " >> " + str(rsp.status_code) +   "\n"
+			
+				if rsp.status_code >=400 :
+					print(urltf)
+					txtlog =txtlog + str(payload) + "\n"
+					print(txtlog =txtlog + str(rsp.json()) + "\n")			 
+#########################
+# LOGOUT SERVICE LAYER
+#########################				
+		urllogout = url + "Logout"
+		rsplogout = appSession.post(urllogout,json=payload,verify=False)		
+
+		self.status = "postSAP"		
 
 
 		
-
+            # self.env["cnw.so.audittrail"].create({
+            #                                     "sonumber":inv.numatcard,
+            #                                     "cardcode":inv.cardcode,
+            #                                     "cardname":inv.cardname, 
+            #                                     "sales":inv.salesperson,
+            #                                     "arperson":inv.arperson,
+            #                                     "docref":inv.docnum,
+            #                                     "docdate":inv.docdate,
+            #                                     "doctype":"INVOICE",
+            #                                     "position":"INVOICE",
+            #                                     "docstatus":"invoice Checklist",
+            #                                     "docby":self.env.user.name ,
+            #                                     "docindate":self.checklist_date})
 
 
